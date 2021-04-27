e5dc8b964d9563d28a8e9d3df62d8f8df453f6e6
Robert Lincoln Truesdale III <31676518+tretrue@users.noreply.github.com>
2021-04-27 12:41:52 -0500

    built out starter character and animations

---
+++ Assets/Character/AnimationController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator _animator;
+    public Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var flatVelocity = body.velocity;
+        flatVelocity.y = 0;
+
+        var flatSpeed = flatVelocity.magnitude;
+        bool isRunning = flatSpeed > 0.001f;
+
+        _animator.SetBool("isRunning", isRunning);
+
+        if (isRunning)
+        {
+            var direction = Quaternion.LookRotation(flatVelocity);
+            body.MoveRotation(direction);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (body.position.y < 1.1)
+            {
+                _animator.SetTrigger("jump");
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            _animator.SetTrigger("slide");
+
+        }
+    }
+}
